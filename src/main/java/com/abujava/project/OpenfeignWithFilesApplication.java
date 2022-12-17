590456d50ab5a6242345a8d05289c14b1a5359f8
Muhammad <shaymanov.2000@gmail.com>
2022-12-17 15:07:32 +0500

    Initial commit

---
+++ src/main/java/com/abujava/project/OpenfeignWithFilesApplication.java
@@ -0,0 +1,13 @@
+package com.abujava.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignWithFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignWithFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abujava/project/OpenfeignWithFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abujava.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignWithFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
